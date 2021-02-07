59cc3f971ba1c8a77f73213f7a83f6963d58769e
Nasir Uddin <monforingnasir@gmail.com>
2021-02-07 23:07:42 +0600

    completed hungry monster project assignment-5

--- js/app.js
+++ js/app.js
@@ -44,31 +44,34 @@ const displayFoodDetail = name => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
     fetch(url)
         .then(res => res.json())
-        .then(data => DetailInfo(data.meals[0]));
+        .then(data => DetailInfo(data.meals));
 }
 
 
 // create function for show food elements
 const DetailInfo = (detailData) => {
-    getDetail.innerHTML = `
-    <img id="detail-img" src = "${detailData.strMealThumb}">
-    <h1>Food Name: ${detailData.strMeal}</h1>
-    <h3>Country-Place: ${detailData.strArea}</h3>
-    <h3>Category: ${detailData.strCategory}</h3>
-    <h2>INGREDIENT</2>
-    <h4>1: ${detailData.strIngredient1}</h4>
-    <h4>2: ${detailData.strIngredient2}</h4>
-    <h4>3: ${detailData.strIngredient3}</h4>
-    <h4>4: ${detailData.strIngredient4}</h4>
-    <h4>5: ${detailData.strIngredient5}</h4>
-    <h4>6: ${detailData.strIngredient6}</h4>
-    <h4>7: ${detailData.strIngredient7}</h4>
-    <h4>8: ${detailData.strIngredient8}</h4>
-    <h4>9: ${detailData.strIngredient9}</h4>
-    <h4>10: ${detailData.strIngredient10}</h4>
-    <h4>11: ${detailData.strIngredient11}</h4>
-    
-     `
+    for (let i = 0; i < detailData.length; i++) {
+        const itemValue = detailData[i];
+        getDetail.innerHTML = `
+        <img id="detail-img" src = "${itemValue.strMealThumb}">
+        <h1>Food Name: ${itemValue.strMeal}</h1>
+        <h3>Country-Place: ${itemValue.strArea}</h3>
+        <h3>Category: ${itemValue.strTags}</h3>
+        <h2>INGREDIENT</2>
+        <h4>1: ${itemValue.strIngredient1}</h4>
+        <h4>2: ${itemValue.strIngredient2}</h4>
+        <h4>3: ${itemValue.strIngredient3}</h4>
+        <h4>4: ${itemValue.strIngredient4}</h4>
+        <h4>5: ${itemValue.strIngredient5}</h4>
+        <h4>6: ${itemValue.strIngredient6}</h4>
+        <h4>7: ${itemValue.strIngredient7}</h4>
+        <h4>8: ${itemValue.strIngredient8}</h4>
+        <h4>9: ${itemValue.strIngredient9}</h4>
+        <h4>10: ${itemValue.strIngredient10}</h4>
+        <h4>11: ${itemValue.strIngredient11}</h4>
+        
+         `
+    }
 }
 
 
