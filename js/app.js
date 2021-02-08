00fd04135a1078f215e698ef9443e0dd4c7b47c2
Nasir Uddin <monforingnasir@gmail.com>
2021-02-08 15:29:49 +0600

    finally completed Hungry Monster Project Assingment-5

--- js/app.js
+++ js/app.js
@@ -6,73 +6,92 @@ const clickHandler = () => {
 
 
 // create function  for get handler value 
-const loadFoodItemData = (inputValue) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
-        .then(promise => promise.json())
-        .then(data => showFoodItemData(data.meals));
+const loadFoodItemData = async (inputValue) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+    try {
+        const promise = await fetch(url);
+        const data = await promise.json();
+        showFoodItemData(data.meals);
+    }
+    catch (error) {
+        getError("We can't find your items!!! Sorry Try Again Later!");
+    }
 
 }
 
 
 // create function for show all body content images and title 
-const bodyContent = document.getElementById('body-content');
 const showFoodItemData = (data) => {
-    data.forEach(ItemList => {
-        const div = document.createElement('div');
-        div.className = 'style-meal'
-        const img = document.createElement('img');
-        img.src = `${ItemList.strMealThumb}`;
-        div.appendChild(img);
-        const h4 = document.createElement('h4');
-        h4.className = "style-name";
-        h4.innerText = ItemList.strMeal;
-        div.appendChild(h4);
-        bodyContent.appendChild(div)
+    const bodyContent = document.getElementById('body-content');
+    bodyContent.innerHTML = '';
+    data.forEach(mealList => {
+        const hungryDiv = document.createElement('div');
+        hungryDiv.className = "single-result row align-items-center my-3 p-3";
+        hungryDiv.innerHTML = `
+        <div onclick="displayFoodDetail('${mealList.strMeal}');" >
+            <div class="col-md-4 hungry-image">
+                <img src="${mealList.strMealThumb}" alt="">
+            </div>
+            <h2>${mealList.strMeal}</h2>
+        </div>
+        `;
+        bodyContent.appendChild(hungryDiv);
     });
 }
 
 
-// create click handler for see details 
-const getDetail = document.getElementById('get-detail')
-bodyContent.addEventListener('click', function (event) {
-    const getId = event.target.innerText;
-    displayFoodDetail(getId)
-});
-
-
-const displayFoodDetail = name => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => DetailInfo(data.meals));
-}
-
-
-// create function for show food elements
-const DetailInfo = (detailData) => {
-    for (let i = 0; i < detailData.length; i++) {
-        const itemValue = detailData[i];
-        getDetail.innerHTML = `
-        <img id="detail-img" src = "${itemValue.strMealThumb}">
-        <h1>Food Name: ${itemValue.strMeal}</h1>
-        <h3>Country-Place: ${itemValue.strArea}</h3>
-        <h3>Category: ${itemValue.strTags}</h3>
-        <h2>INGREDIENT</2>
-        <h4>1: ${itemValue.strIngredient1}</h4>
-        <h4>2: ${itemValue.strIngredient2}</h4>
-        <h4>3: ${itemValue.strIngredient3}</h4>
-        <h4>4: ${itemValue.strIngredient4}</h4>
-        <h4>5: ${itemValue.strIngredient5}</h4>
-        <h4>6: ${itemValue.strIngredient6}</h4>
-        <h4>7: ${itemValue.strIngredient7}</h4>
-        <h4>8: ${itemValue.strIngredient8}</h4>
-        <h4>9: ${itemValue.strIngredient9}</h4>
-        <h4>10: ${itemValue.strIngredient10}</h4>
-        <h4>11: ${itemValue.strIngredient11}</h4>
-        
-         `
+// create function for click handler 
+const displayFoodDetail = async (foodData) => {
+    console.log(foodData);
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodData}`;
+    try {
+        const promise = await fetch(url);
+        const data = await promise.json();
+        showFoodDetails(data.meals[0]);
+    }
+    catch (error) {
+        getError('Sorry!!! We Have no Gradient Item At This Moment');
     }
 }
 
 
-// closing all script tag 
+// creat function for show food details 
+const showFoodDetails = (foodName) => {
+    const getDetail = document.getElementById('get-detail')
+    getDetail.innerHTML = "";
+
+    const foodDetail = document.createElement('div');
+    foodDetail.className = 'food-detail';
+    foodDetail.innerHTML = `
+        <img src="${foodName.strMealThumb}" alt="">
+        <h1>${foodName.strMeal}</h1>
+        <h3>Ingredients:</h3>
+        <ul >
+            <li>${foodName.strIngredient1}</li> 
+            <li>${foodName.strIngredient2}</li> 
+            <li>${foodName.strIngredient3}</li> 
+            <li>${foodName.strIngredient4}</li> 
+            <li>${foodName.strIngredient5}</li> 
+            <li>${foodName.strIngredient6}</li> 
+            <li>${foodName.strIngredient7}</li> 
+            <li>${foodName.strIngredient8}</li> 
+            <li>${foodName.strIngredient9}</li> 
+            <li>${foodName.strIngredient10}</li> 
+            <li>${foodName.strIngredient11}</li>
+        </ul> 
+        `;
+    getDetail.appendChild(foodDetail);
+
+
+
+}
+
+
+// create function for show display error massage 
+const getError = (error) => {
+    const getError = document.getElementById('display-error');
+    getError.innerText = '';
+    getError.innerText = error;
+}
+
+// closing all script file 
